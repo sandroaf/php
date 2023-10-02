df95a418fef87cfbebb01a0cb0a977172300ccdb
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2023-10-02 14:05:35 -0300

    Correções no CSS

--- listacompras/alterarlista.php
+++ listacompras/alterarlista.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
-    <link rel="stylesheet" href="../estilo.css">
+    <link rel="stylesheet" href="./estilo.css">
     <title>Alterar Lista</title>
     <script language="JavaScript">
         function cancelar() {
