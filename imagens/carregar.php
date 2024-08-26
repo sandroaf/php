5d6a8e04705972b9ddcd5694f1d9684d087ac232
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2024-08-26 17:41:54 -0300

    ajustando imagens

--- imagens/carregar.php
+++ imagens/carregar.php
@@ -17,6 +17,7 @@
     // Verifica se é uma imagem real
     if (isset($_POST["submit"])) {
         $check = getimagesize($_FILES["arquivo"]["tmp_name"]);
+        echo "<br>".$_FILES["arquivo"]["tmp_name"]."<br>";
         if ($check !== false) {
             echo "Arquivo é uma imagem - " . $check["mime"] . ".<br>";
             $uploadOk = 1;
@@ -24,42 +25,44 @@
             echo "Arquivo não é uma imagem.<br>";
             $uploadOk = 0;
         }
-    }
 
-    // Verifica se o arquivo já não existe
-    if (file_exists($target_file)) {
-        echo "Desculpe, arquivo já existe<br>";
-        $uploadOk = 0;
-    }
+        // Verifica se o arquivo já não existe
+        if (file_exists($target_file)) {
+            echo "Desculpe, arquivo já existe<br>";
+            $uploadOk = 0;
+        }
 
-    // Check file size
-    if ($_FILES["arquivo"]["size"] > 500000) {
-        echo "Desculpe, arquivo muito grande.<br>";
-        $uploadOk = 0;
-    }
+        // Check file size
+        if ($_FILES["arquivo"]["size"] > 500000) {
+            echo "Desculpe, arquivo muito grande.<br>";
+            $uploadOk = 0;
+        }
 
-    // Allow certain file formats
-    if (
-        $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif"
-    ) {
-        echo "Desculpe, suportado somente JPG, JPEG, PNG & GIF.<br>";
-        $uploadOk = 0;
-    }
+        // Allow certain file formats
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" && $imageFileType != "webp" && $imageFileType != "svg"
+        ) {
+            echo "Desculpe, suportado somente JPG, JPEG, PNG, WEBP, SVG e GIF.<br>";
+            $uploadOk = 0;
+        }
 
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Desculpe, seu arquivo não foi carregado.<br>";
-        // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $target_file)) {
-            echo "The file " . htmlspecialchars(basename($_FILES["arquivo"]["name"])) . " carregado com sucesso.<br>";
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Desculpe, seu arquivo não foi carregado.<br>";
+            // if everything is ok, try to upload file
         } else {
-            echo "Desculpe, ocorreu um erro ao carregar o arquivo.<br>";
+            if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $target_file)) {
+                echo "O arquivo " . htmlspecialchars(basename($_FILES["arquivo"]["name"])) . " carregado com sucesso.<br>";
+                echo "<img src='" . $target_file . "'><br>";
+            } else {
+                echo "Desculpe, ocorreu um erro ao carregar o arquivo.<br>";
+            }
         }
     }
+
     ?>
-    <br> 
+    <br>
     <a href="index.php"><- Voltar</a>
 </body>
 
--- imagens/index.php
+++ imagens/index.php
@@ -5,9 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exemplo carregamento Imagens</title>
+    <script>
+        function abrir(arq) {
+            console.log(arq);
+            document.getElementById("img").innerHTML = "<img src='img/"+arq+"'>";
+        }
+    </script>
 </head>
 
 <body>
+    <h2>Listando Imagens</h2>
+    <?php
+       $pasta = dir('img');
+       echo "Handle: " . $pasta->handle . "<br>";
+       echo "Pasta: " . $pasta->path . "<br>";
+       
+       while (($arq = $pasta->read()) !== false){
+         if ($arq != "." and $arq != "..") {
+            echo "Nome Arquivo: " . $arq . " - <button onclick='abrir(\"$arq\")'>Abrir</button><br>";
+         }
+       }
+       $pasta->close();
+    ?>    
+    <div id="img"></div>
+    <h2>Enviar Imagem</h2>
     <form action="carregar.php" method="post" enctype="multipart/form-data">
         <label for="arquivo">Selecione imagem para upload:</label><br>
         <input type="file" name="arquivo" id="arquivo">
